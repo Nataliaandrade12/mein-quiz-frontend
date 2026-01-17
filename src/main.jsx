c5bddc3c6a955358c88a754f0ef0a6a78e6348b1
Nataliaandrade12 <natalia.fariasdeandrade@wiss-edu.ch>
2026-01-17 09:09:04 +0100

    feat: Wrap App with AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import { AuthProvider } from "./contexts/AuthContext.jsx"; // ← NEU!
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+    <StrictMode>
+        <AuthProvider> {/* ← NEU! AuthProvider umwickelt alles */}
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </AuthProvider>
+    </StrictMode>
 );
